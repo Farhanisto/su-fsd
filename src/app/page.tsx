194a36b33d6476b3e0a6eaca3521b25b3607c546
Farhan <farhanisto@gmail.com>
2024-09-20 15:38:42 +0300

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   }, []);
 
   const sortItems = (method: string) => {
-    let sortedItems = [...items];
+    const sortedItems = [...items];
     if (method === "createdAtAsc") {
       sortedItems.sort(
         (a, b) =>
@@ -41,15 +41,17 @@ export default function Home() {
 
   return (
     <div className="p-5">
-      <select
-        onChange={(e) => sortItems(e.target.value)}
-        value={sortMethod}
-        className="bg-gray-200 text-black p-2 mb-4 rounded"
-      >
-        <option value="createdAtAsc">Sort by Created At Asc</option>
-        <option value="filenameAsc">Sort by Filename Asc</option>
-        <option value="filenameDesc">Sort by Filename Desc</option>
-      </select>
+      <div className="flex justify-center mb-4">
+        <select
+          onChange={(e) => sortItems(e.target.value)}
+          value={sortMethod}
+          className="bg-gray-200 text-black p-2 rounded"
+        >
+          <option value="createdAtAsc">Sort by Created At Asc</option>
+          <option value="filenameAsc">Sort by Filename Asc</option>
+          <option value="filenameDesc">Sort by Filename Desc</option>
+        </select>
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {items.map((item) => (
